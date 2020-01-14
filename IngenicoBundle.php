0bff42e4f7ba28c7a0f441e01fb3493b02619284
Dmytro Fedorenko <dfedorenko@oroinc.com>
2020-01-14 23:28:08 +0200

    INGA-14: Create package with IngenicoBundle (#26506)
    - create ingenico bundle
    - add dependency to commerce-crm-ee and commerce applications

---
+++ IngenicoBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Ingenico\Connect\OroCommerce;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * The IngenicoBundle bundle class.
+ */
+class IngenicoBundle extends Bundle
+{
+}
